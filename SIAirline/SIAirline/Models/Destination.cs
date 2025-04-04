cc5bbed7cbd10a65a62c109643d50c2af5c05726
Arman Bašović <91558948+aNaToMiCaLL@users.noreply.github.com>
2025-04-04 12:28:02 +0200

    Bazni projekat: Pripremljen za migracije
    Projekat je bazno konfigurisan i spreman za razvoj.
    
    Dodan Models folder sa svim klasama koje predstavljaju entitete iz baze podataka.
    Instalirani i usklađeni svi potrebni paketi (EF Core, Pomelo, itd.).
    Implementiran Repository Pattern za CRUD operacije.
    Izvršena uspješna migracija na lokalnu MySQL bazu podataka , generisane su sve tabele prema definisanim modelima.

---
+++ SIAirline/SIAirline/Models/Destination.cs
@@ -0,0 +1,32 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+public enum DestinationType
+{
+    NeZnam,
+    Znam
+}
+    public class Destination
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string CityCode { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string AirportCode { get; set; }
+
+    [Required]
+    public DestinationType Status { get; set; } = DestinationType.NeZnam;
+
+        // Navigation properties for related Flights
+        public ICollection<Flight> DepartureFlights { get; set; }
+        public ICollection<Flight> ArrivalFlights { get; set; }
+    }
---
+++ SIAirline/SIAirline/Models/Flight.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Flight
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string FlightNumber { get; set; }
+
+        [Required]
+        public int AircraftId { get; set; }
+
+        [Required]
+        public int DepartureDestinationId { get; set; }
+
+        [Required]
+        public int ArrivalDestinationId { get; set; }
+
+        [Required]
+        public DateTime DepartureTime { get; set; }
+
+        [Required]
+        public DateTime ArrivalTime { get; set; }
+
+        [Required]
+        public string Status { get; set; } // Use enum if needed
+
+        [Required]
+        public float EconomyPrice { get; set; }
+
+        [Required]
+        public float BusinessPrice { get; set; }
+
+        [Required]
+        public float FirstClassPrice { get; set; }
+
+        [Required]
+        public int Capacity { get; set; }
+
+        [Required]
+        public int AvailableSeats { get; set; }
+
+        // Navigation properties
+        [ForeignKey("AircraftId")]
+        public Aircraft Aircraft { get; set; }
+
+        [ForeignKey("DepartureDestinationId")]
+        public Destination DepartureDestination { get; set; }
+
+        [ForeignKey("ArrivalDestinationId")]
+        public Destination ArrivalDestination { get; set; }
+    }
